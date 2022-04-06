10d7e82bfde75c688deae511d96ce86d01f3f7e7
안민규 <a75311@naver.com>
2022-04-06 23:51:55 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42576

--- Coding/source.cpp
+++ Coding/source.cpp
@@ -3,10 +3,17 @@
 
 using namespace std;
 
-int main() {
-    int answer = 0;
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
 
-    printf("Hello World");
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
 
+    for (int i = 0; i <= completion.size(); i++) {
+        if (participant[i] != completion[i]) {
+            answer = participant[i];
+            break;
+        }
+    }
     return answer;
 }
